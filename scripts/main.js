7356155abcf02f7aed9a343f825f13b259d9e333
maurormrz <maurormrz15@gmail.com>
2023-12-17 12:31:38 -0300

    v-0.0.7
    importacion arreglada

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,1 @@
-import Swal from 'sweetalert2'
-
-var miBoton = document.getElementById('');
-
-  miBoton.addEventListener('click', function() {
-    
-    Swal.fire({
-        title: "Sweet!",
-        text: "Modal with a custom image.",
-        imageUrl: "https://unsplash.it/400/200",
-        imageWidth: 400,
-        imageHeight: 200,
-        imageAlt: "Custom image"
-      });
-    });
+console.log("Hola Mundo")
