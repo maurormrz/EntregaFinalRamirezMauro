eed3db96f428c6e0e0c0fb3e7c21b694662146dc
maurormrz <maurormrz15@gmail.com>
2023-12-17 13:30:02 -0300

    v-0.0.8
    offcanvas solucionado

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,19 @@
 console.log("Hola Mundo")
+
+// Evento de clic para cada tipo de televisor
+document.querySelectorAll('.tv-type').forEach(function (element) {
+    element.addEventListener('click', function () {
+        var tvTitle = this.innerText;
+        var tvImage = this.dataset.image;
+        var tvText = this.getAttribute('data-text');
+
+        Swal.fire({
+            title: tvTitle,
+            text: tvText,
+            imageUrl: tvImage,
+            imageWidth: 300,
+            imageHeight: 300,
+            imageAlt: tvTitle
+        });
+    });
+});
