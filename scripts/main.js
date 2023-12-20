54da228ce960dd70fdbb7a254e9bf32ccf5e0b12
maurormrz <maurormrz15@gmail.com>
2023-12-20 09:49:38 -0300

    v-0.1.2
    sessionStorage agregado

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
+// Carrito de compras
+let carrito = JSON.parse(sessionStorage.getItem('carrito')) || [];
+
+// Cargar carrito desde SessionStorage al cargar la página
+carrito = JSON.parse(sessionStorage.getItem('carrito')) || [];
+actualizarCarrito();
+
 // Evento de clic para cada tipo de televisor
 document.querySelectorAll('.tv-type').forEach(function (element) {
   element.addEventListener('click', function () {
@@ -10,82 +17,86 @@ document.querySelectorAll('.tv-type').forEach(function (element) {
 
 // Lista de productos disponibles
 const productos = {
-plasma: { nombre: "TV Plasma", precio: 30000 },
-lcd: { nombre: "TV LCD", precio: 60000 },
-led: { nombre: "TV LED", precio: 140000 },
-oled: { nombre: "TV OLED", precio: 500000 },
+  plasma: { nombre: "TV Plasma", precio: 30000 },
+  lcd: { nombre: "TV LCD", precio: 60000 },
+  led: { nombre: "TV LED", precio: 140000 },
+  oled: { nombre: "TV OLED", precio: 500000 },
 };
-
-// Carrito de compras
-const carrito = [];
 
 // Función para agregar un producto al carrito
 function agregarAlCarrito(productoId) {
-const producto = productos[productoId];
+  const producto = productos[productoId];
 
-if (producto) {
-  carrito.push(producto);
-  Swal.fire({title: "Añadido al carrito", text: `${producto.nombre} ha sido añadido al carrito.`, icon: "success", timer: 1000, showConfirmButton: false});
-  actualizarCarrito();
-}
+  if (producto) {
+    carrito.push(producto);
+    Swal.fire({title: "Añadido al carrito", text: `${producto.nombre} ha sido añadido al carrito.`, icon: "success", timer: 1000, showConfirmButton: false});
+    actualizarCarrito();
+    actualizarSessionStorage();
+  }
 }
 
 // Función para actualizar la vista del carrito
 function actualizarCarrito() {
-const carritoItems = document.getElementById("carritoItems");
-const totalElement = document.getElementById("total");
+  const carritoItems = document.getElementById("carritoItems");
+  const totalElement = document.getElementById("total");
 
-// Limpiar el contenido del carrito
-carritoItems.innerHTML = "";
+  // Limpiar el contenido del carrito
+  carritoItems.innerHTML = "";
 
-// Calcular el total
-let total = 0;
+  // Calcular el total
+  let total = 0;
 
-// Recorrer los productos en el carrito
-carrito.forEach((producto) => {
-  const listItem = document.createElement("li");
-  listItem.className = "list-group-item d-flex justify-content-between align-items-center";
-  listItem.textContent = `${producto.nombre} $${producto.precio.toFixed(2)}`;
+  // Recorrer los productos en el carrito
+  carrito.forEach((producto) => {
+    const listItem = document.createElement("li");
+    listItem.className = "list-group-item d-flex justify-content-between align-items-center";
+    listItem.textContent = `${producto.nombre} $${producto.precio.toFixed(2)}`;
 
-  // Agregar botón para quitar elemento del carrito
-  const removeButton = document.createElement("button");
-  removeButton.className = "btn btn-danger btn-sm";
-  removeButton.innerHTML = '<i class="bi bi-x"></i>';
-  removeButton.addEventListener("click", () => quitarDelCarrito(producto));
-  
-  listItem.appendChild(removeButton);
-  carritoItems.appendChild(listItem);
+    // Agregar botón para quitar elemento del carrito
+    const removeButton = document.createElement("button");
+    removeButton.className = "btn btn-danger btn-sm";
+    removeButton.innerHTML = '<i class="bi bi-x"></i>';
+    removeButton.addEventListener("click", () => quitarDelCarrito(producto));
+    
+    listItem.appendChild(removeButton);
+    carritoItems.appendChild(listItem);
 
-  // Sumar al total
-  total += producto.precio;
-});
+    // Sumar al total
+    total += producto.precio;
+  });
 
-// Actualizar el total
-totalElement.textContent = `$${total.toFixed(2)}`;
+  // Actualizar el total
+  totalElement.textContent = `$${total.toFixed(2)}`;
 }
 
 // Función para quitar un producto del carrito
 function quitarDelCarrito(producto) {
-const index = carrito.indexOf(producto);
-if (index !== -1) {
-  carrito.splice(index, 1);
-  Swal.fire({title: "Producto eliminado", text: `${producto.nombre} ha sido eliminado del carrito.`, icon: "success", timer: 1000, showConfirmButton: false});
-  actualizarCarrito();
-}
+  const index = carrito.indexOf(producto);
+  if (index !== -1) {
+    carrito.splice(index, 1);
+    Swal.fire({title: "Producto eliminado", text: `${producto.nombre} ha sido eliminado del carrito.`, icon: "success", timer: 1000, showConfirmButton: false});
+    actualizarCarrito();
+    actualizarSessionStorage();
+  }
 }
 
 // Función para vaciar el carrito
 function vaciarCarrito() {
-carrito.length = 0;
-Swal.fire({title: "Carrito vaciado", text: "El carrito ha sido vaciado.", icon: "success", timer: 1000,  showConfirmButton: false});
-actualizarCarrito();
+  carrito.length = 0;
+  Swal.fire({title: "Carrito vaciado", text: "El carrito ha sido vaciado.", icon: "success", timer: 1000,  showConfirmButton: false});
+  actualizarCarrito();
+  actualizarSessionStorage();
+}
+
+// Función para actualizar SessionStorage
+function actualizarSessionStorage() {
+  sessionStorage.setItem('carrito', JSON.stringify(carrito));
 }
 
 // Event listeners
 document.getElementById("botonVaciar").addEventListener("click", vaciarCarrito);
 
 // Opiniones de Usuarios
-
 document.addEventListener('DOMContentLoaded', cargarOpiniones);
 
 function cargarOpiniones() {
