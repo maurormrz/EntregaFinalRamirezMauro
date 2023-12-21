c133bde41869814cd2afb0a145f190ed119e737d
maurormrz <maurormrz15@gmail.com>
2023-12-20 23:27:51 -0300

    v-0.1.5
    Calculadora de cuotas final

--- scripts/main2.js
+++ scripts/main2.js
@@ -7,6 +7,28 @@ document.addEventListener('DOMContentLoaded', function () {
     const cuotasForm = document.getElementById('cuotasForm');
     const precioTotalSpan = document.getElementById('precioTotal');
     const radioCuotas = cuotasForm.elements['cuotas'];
+    const correoInput = document.getElementById('correo');
+    const completarPagoButton = document.getElementById('completarPago');
+
+    // Función para validar el formato del correo
+    function validarCorreo() {
+        const correoValue = correoInput.value;
+        const correoValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(correoValue);
+
+        if (!correoValido) {
+            // Mostrar mensaje de error con SweetAlert2
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Ingrese un correo electrónico válido',
+            });
+        }
+    }
+
+    // Función para calcular el precio en cuotas
+    function calcularPrecioCuotas(cuotas) {
+        return (totalCarrito / cuotas).toFixed(2);
+    }
 
     // Función para manejar el cambio en los radio buttons
     function handleCuotasChange() {
@@ -14,17 +36,56 @@ document.addEventListener('DOMContentLoaded', function () {
         let precioTotal;
 
         if (cuotasSeleccionadas === 1) {
-            precioTotal = totalCarrito;
+            precioTotal = totalCarrito.toFixed(2);
         } else {
-            precioTotal = (totalCarrito / cuotasSeleccionadas).toFixed(2);
+            precioTotal = calcularPrecioCuotas(cuotasSeleccionadas);
         }
 
         // Mostrar el precio total en el span
         precioTotalSpan.textContent = precioTotal;
     }
 
+    // Función para manejar clic en el botón de completar pago
+    function handleCompletarPagoClick() {
+        // Verificar si se seleccionó una opción de cuotas
+        const cuotasSeleccionadas = parseInt(radioCuotas.value);
+        if (isNaN(cuotasSeleccionadas)) {
+            // Mostrar mensaje de error con SweetAlert2
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Seleccione una opción de cuotas',
+            });
+            return;
+        }
+
+        // Verificar si el correo es válido
+        const correoValue = correoInput.value;
+        const correoValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(correoValue);
+
+        if (!correoValido) {
+            // Mostrar mensaje de error con SweetAlert2
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Ingrese un correo electrónico válido',
+            });
+            return;
+        }
+
+        // Si todas las validaciones pasan, mostrar mensaje de éxito con SweetAlert2
+        Swal.fire({
+            icon: 'success',
+            title: '¡Éxito!',
+            text: `Se envió el código de seguimiento al correo: ${correoValue}`,
+        });
+    }
+
     // Agregar evento de cambio a los radio buttons
     cuotasForm.addEventListener('change', handleCuotasChange);
+
+    // Agregar evento de clic al botón de completar pago
+    completarPagoButton.addEventListener('click', handleCompletarPagoClick);
 
     // Inicializar el precio total con el valor original
     precioTotalSpan.textContent = totalCarrito.toFixed(2);
